5c9711aa3bd257e430079b4458305cd55b1385ba
RyukaTV <gabinjuillardpro@gmail.com>
2023-12-04 16:52:48 +0100

    update probleme avec navbar

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\JsonConverter;
+use App\Service\ApiLinker;
+
+class ConnexionController extends AbstractController
+{
+
+    private $jsonConverter;
+    private $apiLinker;
+
+    public function __construct(ApiLinker $apiLinker, JsonConverter $jsonConverter)
+    {
+        $this->apiLinker = $apiLinker;
+        $this->jsonConverter = $jsonConverter;
+    }
+
+    #[Route('/login', methods: ['GET'])]
+    public function displayConnexionPage() {
+        return $this->render('connexion.html.twig', []);
+    }
+
+    #[Route('/inscription', methods: ['GET'])]
+    public function displayInscriptionPage() {
+        return $this->render('inscription.html.twig', []);
+    }
+
+    #[Route('/login', methods: ['POST'])]
+    public function connexion(Request $request): Response
+    {
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+        if (empty($username) || empty($password)) {
+            return new Response('Les champs username et password sont obligatoires.', Response::HTTP_BAD_REQUEST);
+        }
+
+        $data = $this->jsonConverter->encodeToJson(['username' => $username, 'password' => $password]);
+        $response = $this->apiLinker->postData('/login', $data, null);
+        $responseObject = json_decode($response);
+        
+        $jsonUser = $this->apiLinker->getData('/users/search?username='.$username, $responseObject->token);
+        $user = json_decode($jsonUser); 
+        if ($user->ban == true){
+            return $this->redirect("/logout");
+        }
+        if (!empty($responseObject->token)) {
+            $session = $request->getSession();
+            $session->set('token-session', $responseObject->token);
+
+            return $this->redirect('/');
+        } else {
+            return new Response('Réponse API invalide.:'.var_dump($response), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    #[Route('/logout', methods: ['GET'])]
+    public function deconnexion(Request $request)
+    {
+        $session = $request->getSession();
+        $session->remove('token-session');
+        $session->clear();
+
+        return $this->redirect('/');
+    }
+
+
+    #[Route('/inscription', methods: ['POST'])]
+    public function inscription(Request $request): Response
+    {
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+        $confirm_password = $request->request->get("confirm_password");
+        if (empty($username) || empty($password) || empty($confirm_password)) {
+            return new Response('Les champs username, password et confirmation password sont obligatoires.', Response::HTTP_BAD_REQUEST);
+        }
+        if ($password === $confirm_password) {
+            try {
+                $data = $this->jsonConverter->encodeToJson(['username' => $username, 'password' => $password]);
+                $this->apiLinker->postData('/inscription', $data, null);
+                return $this->redirect("/");
+            } catch (\Exception $e) {
+                return new Response('Erreur lors de la communication avec l\'API : ' . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new Response('Les champs password et confirmation password ne sont pas identique.', Response::HTTP_BAD_REQUEST);
+        }
+
+    }
+}
---
+++ src/Controller/PageController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\JsonConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Service\ApiLinker;
+
+
+class PageController extends AbstractController {
+
+    private $apiLinker;
+    private $jsonConverter;
+  
+    public function __construct(ApiLinker $apiLinker, JsonConverter $jsonConverter) {
+        $this->apiLinker = $apiLinker;
+        $this->jsonConverter= $jsonConverter;
+    }
+
+    #[Route("/", methods: ["GET"])]
+    public function displayAccueilPage(Request $request){
+        $session = $request->getSession();
+        $token = $session->get('token-session');
+
+        $jsonposts= $this->apiLinker->getData("/posts", $token);
+        $posts= json_decode($jsonposts);
+        shuffle($posts);
+        return $this->render("accueil.html.twig", ["token"=>$token, "posts"=>$posts]);
+    }
+
+    #[Route('/myself', methods: ['GET'], condition: "service('route_checker').checkUser(request)")]
+    public function displayUserInfoPage(Request $request) {
+        $session = $request->getSession();
+        $token = $session->get('token-session');
+        $self = $this->apiLinker->getData('/myself', $token);
+        $selfuser = json_decode($self); 
+        $jsonuser= $this->apiLinker->getData("/users/search?username=".$selfuser->username, $token);
+        $user= json_decode($jsonuser);
+
+        return $this->render('searchuser.html.twig', ['user' => $user, "token" => $token, "myself"=>"My Profile"]);
+    }
+
+    #[Route('/users', methods: ['GET'], condition: "service('route_checker').checkAdmin(request)")]
+    public function displayUtilisateursPage(Request $request) {
+        $session = $request->getSession();
+        $token = $session->get('token-session');
+
+        $response = $this->apiLinker->getData('/user', $token);
+        $users = json_decode($response);
+
+        return $this->render('accueil.html.twig', ['users' => $users]);
+    }
+
+    #[Route('/search', methods: ['GET'])]
+    public function getuserbyusername(Request $request): Response
+    {
+        $username = $request->query->get('username');
+
+        if (empty($username)) {
+            return new Response('Le champ username est obligatoire.', Response::HTTP_BAD_REQUEST);
+        }
+        $session = $request->getSession();
+        $token = $session->get('token-session');
+        try {
+            $response = $this->apiLinker->getData('/users/search?username=' . $username, $token);
+            if ($response) {
+                $param= json_decode($response);
+                return $this->render("searchuser.html.twig", ["user" => $param, "token" => $token]);
+            } else {
+                return new JsonResponse(['error' => 'Le champ username est obligatoire.'], Response::HTTP_BAD_REQUEST);
+
+            }
+        } catch (\Exception $e) {
+            return new Response('Erreur lors de la communication avec l\'API.:'.$e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
